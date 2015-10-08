78ced6b4544d6ee858d9fd1c705f5f781c936c62
houseyoung <houseyoung@qq.com>
2015-10-08 18:04:01 +0800

    上传数据库SQL文件。

---
+++ db/t_student.sql
@@ -0,0 +1,139 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.1.53-community-log : Database - db_student
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_student` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `db_student`;
+
+/*Table structure for table `t_admin` */
+
+DROP TABLE IF EXISTS `t_admin`;
+
+CREATE TABLE `t_admin` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `role` int(1) NOT NULL COMMENT '管理员角色：1、全局管理员，2、班级管理员',
+  `class_id` int(10) DEFAULT NULL COMMENT '班级ID（仅班级管理员需要）',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `t_admin` */
+
+/*Table structure for table `t_class` */
+
+DROP TABLE IF EXISTS `t_class`;
+
+CREATE TABLE `t_class` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '班级名称',
+  `department_id` int(10) NOT NULL COMMENT '系ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `t_class` */
+
+/*Table structure for table `t_course` */
+
+DROP TABLE IF EXISTS `t_course`;
+
+CREATE TABLE `t_course` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '课程名称',
+  `class_hour` int(10) DEFAULT NULL COMMENT '课时',
+  `credit` int(10) DEFAULT NULL COMMENT '学分',
+  `place` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '上课地点',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `t_course` */
+
+/*Table structure for table `t_department` */
+
+DROP TABLE IF EXISTS `t_department`;
+
+CREATE TABLE `t_department` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '系名称',
+  `school_id` int(10) NOT NULL COMMENT '学院ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `t_department` */
+
+/*Table structure for table `t_health` */
+
+DROP TABLE IF EXISTS `t_health`;
+
+CREATE TABLE `t_health` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `student_id` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT '学号',
+  `height` int(3) DEFAULT NULL COMMENT '身高',
+  `weight` int(3) DEFAULT NULL COMMENT '体重',
+  `nutrition` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '营养状况',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `t_health` */
+
+/*Table structure for table `t_school` */
+
+DROP TABLE IF EXISTS `t_school`;
+
+CREATE TABLE `t_school` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_turkish_ci NOT NULL COMMENT '学院名称',
+  `university` varchar(50) COLLATE utf8_turkish_ci NOT NULL COMMENT '学校名称',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+/*Data for the table `t_school` */
+
+/*Table structure for table `t_score` */
+
+DROP TABLE IF EXISTS `t_score`;
+
+CREATE TABLE `t_score` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `course_id` int(10) NOT NULL COMMENT '课程ID',
+  `student_id` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT '学号',
+  `score` int(3) NOT NULL COMMENT '成绩',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `t_score` */
+
+/*Table structure for table `t_student` */
+
+DROP TABLE IF EXISTS `t_student`;
+
+CREATE TABLE `t_student` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `student_id` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT '学号',
+  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '登录密码',
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '姓名',
+  `sex` int(1) NOT NULL COMMENT '性别：1、男 2、女',
+  `class_id` int(10) NOT NULL COMMENT '班级ID',
+  `department_id` int(10) NOT NULL COMMENT '系ID',
+  `school_id` int(10) NOT NULL COMMENT '学院ID',
+  `birthday` datetime DEFAULT NULL COMMENT '生日',
+  `hometown` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '籍贯',
+  `interest` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '爱好',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `t_student` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
