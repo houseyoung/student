37b7221d76021aa8231787b0eff3a9861b127021
houseyoung <houseyoung@qq.com>
2015-10-10 17:51:49 +0800

    修改数据库：将学号设为唯一索引

--- db/db_student.sql
+++ db/db_student.sql
@@ -1,12 +1,25 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.1.53-community-log : Database - db_student
+*********************************************************************
+*/
 
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_student` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `db_student`;
 
--- ----------------------------
---  Table structure for `t_admin`
--- ----------------------------
+/*Table structure for table `t_admin` */
+
 DROP TABLE IF EXISTS `t_admin`;
+
 CREATE TABLE `t_admin` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `role` int(1) NOT NULL COMMENT '管理员角色：1、全局管理员，2、班级管理员',
@@ -14,10 +27,12 @@ CREATE TABLE `t_admin` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- ----------------------------
---  Table structure for `t_class`
--- ----------------------------
+/*Data for the table `t_admin` */
+
+/*Table structure for table `t_class` */
+
 DROP TABLE IF EXISTS `t_class`;
+
 CREATE TABLE `t_class` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `class_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '班级名称',
@@ -25,17 +40,14 @@ CREATE TABLE `t_class` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- ----------------------------
---  Records of `t_class`
--- ----------------------------
-BEGIN;
-INSERT INTO `t_class` VALUES ('1', '软件1班', '1'), ('2', '软件2班', '1'), ('3', '软件3班', '1'), ('7', '图书馆1班', '8'), ('9', '计算机1班', '4'), ('13', '计算机3班', '4');
-COMMIT;
+/*Data for the table `t_class` */
 
--- ----------------------------
---  Table structure for `t_course`
--- ----------------------------
+insert  into `t_class`(`id`,`class_name`,`department_id`) values (1,'软件1班',1),(2,'软件2班',1),(3,'软件3班',1),(7,'图书馆1班',8),(9,'计算机1班',4),(13,'计算机3班',4);
+
+/*Table structure for table `t_course` */
+
 DROP TABLE IF EXISTS `t_course`;
+
 CREATE TABLE `t_course` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `course_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '课程名称',
@@ -46,17 +58,14 @@ CREATE TABLE `t_course` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- ----------------------------
---  Records of `t_course`
--- ----------------------------
-BEGIN;
-INSERT INTO `t_course` VALUES ('1', 'C++程序设计', '30', '3', '周东岱', '314');
-COMMIT;
+/*Data for the table `t_course` */
 
--- ----------------------------
---  Table structure for `t_department`
--- ----------------------------
+insert  into `t_course`(`id`,`course_name`,`course_hour`,`credit`,`teacher_name`,`place`) values (1,'C++程序设计',30,3,'周东岱','314');
+
+/*Table structure for table `t_department` */
+
 DROP TABLE IF EXISTS `t_department`;
+
 CREATE TABLE `t_department` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `department_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '系名称',
@@ -64,17 +73,14 @@ CREATE TABLE `t_department` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- ----------------------------
---  Records of `t_department`
--- ----------------------------
-BEGIN;
-INSERT INTO `t_department` VALUES ('1', '软件工程', '1'), ('4', '计算机应用技术', '2'), ('8', '图书馆学', '2');
-COMMIT;
+/*Data for the table `t_department` */
 
--- ----------------------------
---  Table structure for `t_health`
--- ----------------------------
+insert  into `t_department`(`id`,`department_name`,`school_id`) values (1,'软件工程',1),(4,'计算机应用技术',2),(8,'图书馆学',2);
+
+/*Table structure for table `t_health` */
+
 DROP TABLE IF EXISTS `t_health`;
+
 CREATE TABLE `t_health` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `student_id` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT '学号',
@@ -84,10 +90,12 @@ CREATE TABLE `t_health` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- ----------------------------
---  Table structure for `t_school`
--- ----------------------------
+/*Data for the table `t_health` */
+
+/*Table structure for table `t_school` */
+
 DROP TABLE IF EXISTS `t_school`;
+
 CREATE TABLE `t_school` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `school_name` varchar(50) COLLATE utf8_turkish_ci NOT NULL COMMENT '学院名称',
@@ -95,17 +103,14 @@ CREATE TABLE `t_school` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
 
--- ----------------------------
---  Records of `t_school`
--- ----------------------------
-BEGIN;
-INSERT INTO `t_school` VALUES ('1', '信息与软件工程学院', '东北师范大学'), ('2', '计算机科学与信息技术学院', '东北师范大学'), ('7', '传媒科学学院', '东北师范大学');
-COMMIT;
+/*Data for the table `t_school` */
 
--- ----------------------------
---  Table structure for `t_score`
--- ----------------------------
+insert  into `t_school`(`id`,`school_name`,`university`) values (1,'信息与软件工程学院','东北师范大学'),(2,'计算机科学与信息技术学院','东北师范大学'),(7,'传媒科学学院','东北师范大学');
+
+/*Table structure for table `t_score` */
+
 DROP TABLE IF EXISTS `t_score`;
+
 CREATE TABLE `t_score` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `course_id` int(10) NOT NULL COMMENT '课程ID',
@@ -114,74 +119,74 @@ CREATE TABLE `t_score` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- ----------------------------
---  Table structure for `t_student`
--- ----------------------------
+/*Data for the table `t_score` */
+
+/*Table structure for table `t_student` */
+
 DROP TABLE IF EXISTS `t_student`;
+
 CREATE TABLE `t_student` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `student_id` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT '学号',
-  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '登录密码',
+  `student_id` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT '学号',
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT '登录密码',
   `student_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '姓名',
   `sex` varchar(1) COLLATE utf8_unicode_ci NOT NULL COMMENT '性别',
   `class_id` int(10) NOT NULL COMMENT '班级ID',
   `birthday` date DEFAULT NULL COMMENT '生日',
   `hometown` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '籍贯',
   `interest` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '爱好',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQUE` (`student_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- ----------------------------
---  Records of `t_student`
--- ----------------------------
-BEGIN;
-INSERT INTO `t_student` VALUES ('1', '2011012488', '2011012488', '杨昌昊', '男', '1', '1993-01-01', '山东省济南市', '计算机'), ('3', '2011012002', '2011012001', '李四', '女', '1', '1992-05-05', '山东省青岛市', '暂无');
-COMMIT;
+/*Data for the table `t_student` */
 
--- ----------------------------
---  Table structure for `t_user`
--- ----------------------------
+insert  into `t_student`(`id`,`student_id`,`password`,`student_name`,`sex`,`class_id`,`birthday`,`hometown`,`interest`) values (1,'2011012488','827ccb0eea8a706c4c34a16891f84e7b','杨昌昊','男',1,'1993-01-01','山东省济南市','计算机'),(3,'2011012002','c0da6cbc0094c8bf7edad2d296a9ebe9','李四','女',1,'1992-05-05','山东省青岛市','暂无'),(17,'2011012001','827ccb0eea8a706c4c34a16891f84e7b','张三','男',7,'1993-06-06','北京市','吃饭');
+
+/*Table structure for table `t_user` */
+
 DROP TABLE IF EXISTS `t_user`;
+
 CREATE TABLE `t_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` varchar(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
--- ----------------------------
---  Records of `t_user`
--- ----------------------------
-BEGIN;
-INSERT INTO `t_user` VALUES ('3', 'admin', '21232f297a57a5a743894a0e4a801fc3', '1'), ('4', 'role2', '84ccdc5607f6af253d3ad709b382ef3f', '2');
-COMMIT;
+/*Data for the table `t_user` */
 
--- ----------------------------
---  Procedure structure for `1`
--- ----------------------------
-DROP PROCEDURE IF EXISTS `1`;
-delimiter ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `1`()
+insert  into `t_user`(`id`,`username`,`password`,`role`) values (3,'admin','21232f297a57a5a743894a0e4a801fc3','1'),(4,'role2','84ccdc5607f6af253d3ad709b382ef3f','2'),(5,'123','202cb962ac59075b964b07152d234b70','1');
+
+/* Procedure structure for procedure `1` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `1` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `1`()
     DETERMINISTIC
 INSERT INTO t_class (class_name, department_id)
         VALUES ("计算机哈哈哈",
         (SELECT t_department.id FROM t_department
           WHERE department_name = "计算机应用技术"
           AND t_school.school_name = "计算机科学与信息技术学院"
-          ))
- ;;
-delimiter ;
+          )) */$$
+DELIMITER ;
 
--- ----------------------------
---  Procedure structure for `2`
--- ----------------------------
-DROP PROCEDURE IF EXISTS `2`;
-delimiter ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `2`()
+/* Procedure structure for procedure `2` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `2` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `2`()
 SELECT id, school_id FROM t_department WHERE department_name = "软件工程"
-        AND school_id = (SELECT id FROM t_school WHERE school_name = "信息与软件工程学院")
- ;;
-delimiter ;
+        AND school_id = (SELECT id FROM t_school WHERE school_name = "信息与软件工程学院") */$$
+DELIMITER ;
 
-SET FOREIGN_KEY_CHECKS = 1;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
