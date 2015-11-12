9d3a5a3f1f5c92d20e50d56759c100851dc0e8fe
houseyoung <houseyoung@qq.com>
2015-11-12 16:47:39 +0800

    删除WebUploader，自己写了一个简单的页面来测试文件上传功能。

--- src/main/java/com/houseyoung/student/controller/FileUploadController.java
+++ src/main/java/com/houseyoung/student/controller/FileUploadController.java
@@ -1,44 +1,46 @@
-//package com.houseyoung.student.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.ModelMap;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import java.io.File;
-//
-///**
-// * Created by houseyoung on 2015/9/28 17:09.
-// * 文件上传Controller
-// */
-//@RequestMapping(value = "")
-//@Controller
-//public class FileUploadController {
-//    @RequestMapping(value = "fileUpload")
-//    public String upload(@RequestParam(value = "file", required = false)  MultipartFile file, HttpServletRequest request, ModelMap model) {
-//        String path = request.getSession().getServletContext().getRealPath("upload");
-//        String fileName = file.getOriginalFilename();
-//        File targetFile = new File(path, fileName);
-//        //若目录不存在则创建之
-//        if(!targetFile.exists()){
-//            targetFile.mkdirs();
-//        }
-//
-//        //保存文件
-//        try {
-//            file.transferTo(targetFile);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        model.addAttribute("fileUrl", request.getContextPath()+"/upload/"+fileName);
-//
-//        return "webuploader";
-//    }
-//
-//    @RequestMapping(value = "webuploader")
-//    public String toWebUploader(){
-//        return "webuploader";
-//    }
-//}
+package com.houseyoung.student.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+
+/**
+ * Created by houseyoung on 2015/9/28 17:09.
+ * 文件上传Controller
+ */
+@RequestMapping(value = "")
+@Controller
+public class FileUploadController {
+    @RequestMapping(value = "fileUpload")
+    public String upload(@RequestParam(value = "file", required = false)  MultipartFile file, HttpServletRequest request, ModelMap model) {
+        String path = request.getSession().getServletContext().getRealPath("resources/upload");
+        String fileName = file.getOriginalFilename();
+        File targetFile = new File(path, fileName);
+        //若目录不存在则创建之
+        if(!targetFile.exists()){
+            targetFile.mkdirs();
+        }
+        //保存文件
+        try {
+            file.transferTo(targetFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //返回文件地址
+        model.addAttribute("fileUrl", request.getContextPath() + "/resources/upload/" + fileName);
+        //返回文件名
+        model.addAttribute("fileName", fileName);
+
+        return "upload";
+    }
+
+    @RequestMapping(value = "upload")
+    public String toFileUpload(){
+        return "upload";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/upload.jsp
@@ -0,0 +1,29 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: house
+  Date: 2015/11/12
+  Time: 16:04
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>上传文件</title>
+    <script type="text/javascript" src="${website}resources/tool/jquery/jquery.js"></script>
+    <script>
+        $(document).ready(function () {
+            var fileUrl = '<%=request.getAttribute("fileUrl") == null ? "" : request.getAttribute("fileUrl")%>';
+            if (fileUrl != null && fileUrl != '') {
+                var text = "<br>上传成功!</br>";
+                var link = "<a href='${fileUrl}'>${fileName}</a>";
+                $("body").append(text, link);
+            }
+        });
+    </script>
+</head>
+<body>
+<form action="${website}fileUpload" method="post" enctype="multipart/form-data">
+    <input type="file" name="file"/> <input type="submit" value="Submit"/>
+</form>
+</body>
+</html>
